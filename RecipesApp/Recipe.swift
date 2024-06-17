310e17f4a785b5777711acdbbab250d310938574
AaronGarman <aaron.a.garman14@gmail.com>
2024-06-17 13:42:34 -0400

    Added base struct files
    Starter files for main structs to use in project.

---
+++ RecipesApp/Recipe.swift
@@ -0,0 +1,16 @@
+//
+//  Recipe.swift
+//  RecipesApp
+//
+//  Created by Aaron Garman on 6/17/24.
+//
+
+import Foundation
+
+struct Recipe {
+    var name: String
+    var description: String
+    var prepTime: String
+    
+    // add more data items as need
+}
---
+++ RecipesApp/ShopItem.swift
@@ -0,0 +1,15 @@
+//
+//  ShopItem.swift
+//  RecipesApp
+//
+//  Created by Aaron Garman on 6/17/24.
+//
+
+import Foundation
+
+struct ShopItem {
+    let name: String
+    let amount: Int
+    
+    // need any other data? prob not
+}
