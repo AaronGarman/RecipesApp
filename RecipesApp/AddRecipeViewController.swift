82b453b800ce75e03b5a444afba125ba7f7105c6
AaronGarman <aaron.a.garman14@gmail.com>
2024-07-09 19:01:49 -0400

    Added base UI elements for add recipe and add shopping list item screens

--- RecipesApp/AddRecipeViewController.swift
+++ RecipesApp/AddRecipeViewController.swift
@@ -9,12 +9,19 @@ import UIKit
 
 class AddRecipeViewController: UIViewController {
 
+    @IBOutlet weak var directionsTextView: UITextView!
+    
     @IBAction func didTapCancelButton(_ sender: Any) {
         dismiss(animated: true)
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Add border to the UITextView - keep v make as rules in storyboard? sends warning in console?
+        directionsTextView.layer.borderColor = UIColor.black.cgColor
+        directionsTextView.layer.borderWidth = 1.0
+        directionsTextView.layer.cornerRadius = 5.0 // Optional, for rounded corners
+         
         // Do any additional setup after loading the view.
     }
     
