5c8db2a4664f96b946ab2a98a8b5a8d827ae8e3f
AaronGarman <aaron.a.garman14@gmail.com>
2024-07-09 18:21:02 -0400

    Setup screen archetypes for adding recipes and shopping list items

---
+++ RecipesApp/AddShopItemViewController.swift
@@ -0,0 +1,34 @@
+//
+//  AddShopItemViewController.swift
+//  RecipesApp
+//
+//  Created by Aaron Garman on 7/9/24.
+//
+
+import UIKit
+
+class AddShopItemViewController: UIViewController {
+
+    
+    @IBAction func didTapCancelButton(_ sender: Any) {
+        dismiss(animated: true)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
