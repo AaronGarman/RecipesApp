624c538595a38820d244ebc5b9ab7389cc77c5f4
AaronGarman <aaron.a.garman14@gmail.com>
2025-06-04 14:37:17 -0400

    Cleaned up + refactored recipes vc

--- RecipesApp/View Controllers/RecipesViewController.swift
+++ RecipesApp/View Controllers/RecipesViewController.swift
@@ -17,12 +17,10 @@ class RecipesViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        recipes = Recipe.mockedRecipes
+        recipes = Recipe.mockedRecipes // out for testing db
         
         recipesTableView.dataSource = self
         recipesTableView.delegate = self
-        
-        navigationController?.navigationBar.prefersLargeTitles = true
         
         menuInit()
     }
@@ -33,56 +31,46 @@ class RecipesViewController: UIViewController {
         if let selectedIndexPath = recipesTableView.indexPathForSelectedRow {
             recipesTableView.deselectRow(at: selectedIndexPath, animated: animated)
         }
-        
-        /// query db here, also reload table view? query and remove parallel v just query each time?
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "AddRecipeSegue" {
-            if let addRecipeNavController = segue.destination as? UINavigationController,
-               let addRecipeViewController = addRecipeNavController.topViewController as? AddRecipeViewController {
-                
-                // Passing recipe to edit if provided
-                if let recipeToEdit = sender as? Recipe {
-                    addRecipeViewController.recipeToEdit = recipeToEdit
+            guard let addRecipeNavController = segue.destination as? UINavigationController,
+                  let addRecipeViewController = addRecipeNavController.topViewController as? AddRecipeViewController else { return }
+            
+            if let recipeToEdit = sender as? Recipe {
+                addRecipeViewController.recipeToEdit = recipeToEdit
+            }
+            
+            addRecipeViewController.onAddRecipe = { [weak self] recipe in
+                if let index = self?.recipes.firstIndex(where: { $0.id == recipe.id }) {
+                    self?.recipes[index] = recipe
+                }
+                else {
+                    self?.recipes.append(recipe)
                 }
                 
-                addRecipeViewController.onAddRecipe = { [weak self] recipe in
-                    
-                    // Check if the recipe exists and determine action
-                    if let index = self?.recipes.firstIndex(where: { $0.id == recipe.id }) {
-                        // Update existing recipe
-                        self?.recipes[index] = recipe
-                    }
-                    else {
-                        // Add new recipe
-                        self?.recipes.append(recipe)
-                    }
-                    
-                    // Refresh the table view
-                    self?.recipesTableView.reloadSections(IndexSet(integer: 0), with: .automatic)
-                }
+                self?.recipesTableView.reloadSections(IndexSet(integer: 0), with: .automatic)
             }
         }
-        else if segue.identifier == "RecipeDetailSegue" {
+        else if segue.identifier == "RecipeDetailSegue" { // maybe do if let? gpt?
             guard let selectedIndexPath = recipesTableView.indexPathForSelectedRow else { return }
             let selectedRecipe = recipes[selectedIndexPath.row]
             guard let recipeDetailViewController = segue.destination as? RecipeDetailViewController else { return }
             recipeDetailViewController.recipe = selectedRecipe
         }
     }
-    
-    func menuInit() { // "sign out vs log out", person v person.circle
+
+    private func menuInit() {
         let menuItems = UIMenu(options: .displayInline, children: [
-            UIAction(title: "Sign Out", image: UIImage(systemName: "rectangle.portrait.and.arrow.right"), handler: { _ in self.showConfirmLogoutAlert() })
+            UIAction(title: "Sign out", image: UIImage(systemName: "rectangle.portrait.and.arrow.right"), handler: { _ in self.showConfirmLogoutAlert() })
         ])
         
-        // Assign menu to button
         settingsButton.menu = menuItems
     }
     
-    func showConfirmLogoutAlert() {
-        let alertController = UIAlertController(title: "Sign Out of Your Account?", message: nil, preferredStyle: .alert)
+    private func showConfirmLogoutAlert() {
+        let alertController = UIAlertController(title: "Sign out of your account?", message: nil, preferredStyle: .alert)
         let logOutAction = UIAlertAction(title: "Sign out", style: .destructive) { _ in
             NotificationCenter.default.post(name: Notification.Name("signOut"), object: nil)
         }
@@ -102,7 +90,8 @@ extension RecipesViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = recipesTableView.dequeueReusableCell(withIdentifier: "RecipeCell", for: indexPath) as? RecipeCell else {
-            fatalError("Unable to dequeue Recipe Cell")
+            print("Unable to dequeue ShopItemCell")
+            return UITableViewCell()
         }
         
         cell.configure(with: recipes[indexPath.row])
@@ -116,6 +105,7 @@ extension RecipesViewController: UITableViewDataSource, UITableViewDelegate {
             self?.performSegue(withIdentifier: "AddRecipeSegue", sender: selectedRecipe)
             completionHandler(true)
         }
+        
         detailAction.backgroundColor = .orange
 
         let configuration = UISwipeActionsConfiguration(actions: [detailAction])
@@ -126,12 +116,7 @@ extension RecipesViewController: UITableViewDataSource, UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-                // Update the data model
                 recipes.remove(at: indexPath.row)
-            
-                /// remove from db here
-                
-                // Delete the row from the table view
                 tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
