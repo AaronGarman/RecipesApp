e446b0d4bd37f7e41b1407d684f87f2a30568619
AaronGarman <aaron.a.garman14@gmail.com>
2025-07-28 17:44:51 -0400

    Added table view cell image loading

--- RecipesApp/View Controllers/RecipeCell.swift
+++ RecipesApp/View Controllers/RecipeCell.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Alamofire
+import AlamofireImage
 
 class RecipeCell: UITableViewCell {
 
@@ -13,9 +15,35 @@ class RecipeCell: UITableViewCell {
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var prepTimeLabel: UILabel!
     
+    private var imageDataRequest: DataRequest?
+    
     func configure(with recipe: Recipe) {
         nameLabel.text = recipe.name
         prepTimeLabel.text = "Prep Time: \(recipe.prepTime) mins"
         //recipeImageView.image = recipe.image // Fix for query db
+        
+        if let imageFile = recipe.imageFile,
+           let imageUrl = imageFile.url {
+            
+            // Use AlamofireImage helper to fetch remote image from URL
+            imageDataRequest = AF.request(imageUrl).responseImage { [weak self] response in
+                switch response.result {
+                case .success(let image):
+                    // Set image view image with fetched image
+                    self?.recipeImageView.image = image
+                case .failure(let error):
+                    print("Error fetching image: \(error.localizedDescription)")
+                    break
+                }
+            }
+        }
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        recipeImageView.image = nil
+        imageDataRequest?.cancel()
+
     }
 }
